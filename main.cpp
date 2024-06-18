9175a261117a7372f275bd894a1c19a4b1560d4d
Edca1 <2914715815@qq.com>
2024-06-18 22:01:35 +0800

    Calendar demo1

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    //const QUrl url(QStringLiteral("qrc:/Desktop_calendar/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("Desktop_calendar", "Window");
+    engine.loadFromModule("Desktop_Calendar", "Window");
 
     return app.exec();
 }
