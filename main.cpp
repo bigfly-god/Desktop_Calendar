7ccb400da147574682eb9ae580ace4ec376ee810
Edca1 <2914715815@qq.com>
2024-06-18 12:01:46 +0800

    version 1.1

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Desktop_calendar/Main.qml"));
+    //const QUrl url(QStringLiteral("qrc:/Desktop_calendar/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.load(url);
+    engine.loadFromModule("Desktop_calendar", "Main");
 
     return app.exec();
 }
