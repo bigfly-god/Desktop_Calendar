a9b06fd404efbd027f57569db51820a3b8cc70c9
bigfly-god <2112084439@qq.com>
2024-06-18 22:43:16 +0800

    Calendar

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
